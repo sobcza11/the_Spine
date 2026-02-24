2d4886166484a69bcb22000a911607530c024caa
Stan <rand.sobczak@gmail.com>
2026-02-24 18:09:38 -0500

    Add setup.py to allow installation of spine module

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spine",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        # Add any dependencies your project needs, like:
+        # 'numpy',
+        # 'pandas',
+        # 'requests',
+    ],
+)
