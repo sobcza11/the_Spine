56f873c252bce7276ce6eb60f343d0207bf4305e
Stan <rand.sobczak@gmail.com>
2026-06-29 10:10:55 -0400

    Separate refresh registry from dispatcher

---
+++ src/spine/jobs/geoscen/cb/refresh_registry.py
@@ -0,0 +1,40 @@
+############################################################
+# refresh_registry.py
+############################################################
+
+BANK_REGISTRY = {
+
+    "FED": {
+        "name": "Federal Reserve",
+        "pipeline": [
+            "spine.jobs.geoscen.fomc.ingest_fomc_minutes_t1",
+            "spine.jobs.geoscen.fomc.validate_fomc_minutes_t1",
+            "spine.jobs.geoscen.fomc.historical.ingest_fomc_historical_materials_t1",
+            "spine.jobs.geoscen.fomc.historical.validate_fomc_historical_materials_t1",
+            "spine.jobs.geoscen.fomc.historical.extract_fomc_historical_pdf_text_v2",
+            "spine.jobs.geoscen.fomc.historical.validate_fomc_historical_pdf_text_v2",
+            "spine.jobs.geoscen.fomc.upload_fomc_minutes_to_r2",
+            "spine.jobs.geoscen.fomc.historical.upload_fomc_historical_materials_to_r2",
+        ],
+    },
+
+    "ECB": {
+        "name": "European Central Bank",
+        "pipeline": [
+            "spine.jobs.geoscen.cb.ecb.ingest_ecb_policy_decisions_t1",
+            "spine.jobs.geoscen.cb.ecb.validate_ecb_policy_decisions_t1",
+            "spine.jobs.geoscen.cb.ecb.upload_ecb_policy_decisions_to_r2",
+            "spine.jobs.geoscen.cb.ecb.ingest_ecb_accounts_t1",
+            "spine.jobs.geoscen.cb.ecb.validate_ecb_accounts_t1",
+            "spine.jobs.geoscen.cb.ecb.upload_ecb_accounts_to_r2",
+            "spine.jobs.geoscen.cb.ecb.build_ecb_combined_canonical_v1",
+            "spine.jobs.geoscen.cb.ecb.validate_ecb_combined_canonical_v1",
+            "spine.jobs.geoscen.cb.ecb.upload_ecb_combined_canonical_to_r2",
+            "spine.jobs.geoscen.cb.build_macro_cb_canonical_v1",
+            "spine.jobs.geoscen.cb.validate_macro_cb_canonical_v1",
+            "spine.jobs.geoscen.cb.upload_macro_cb_canonical_to_r2",
+        ],
+    },
+
+}
+
